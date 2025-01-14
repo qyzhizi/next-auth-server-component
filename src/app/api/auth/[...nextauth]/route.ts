495285eb1715fc29947c8a725ced6214446b5739
qyzhizi <2830942138@qq.com>
2025-01-15 00:47:34 +0800

    update callback

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,6 +3,7 @@ import GithubProvider from 'next-auth/providers/github'
 import GoogleProvider from 'next-auth/providers/google'
 
 const handler = NextAuth({
+    debug: true,
     providers: [
         GithubProvider({
             clientId: process.env.GITHUB_ID || '',
@@ -21,7 +22,22 @@ const handler = NextAuth({
             console.log(email); // 如果使用邮件登录，包含邮箱信息
             console.log(credentials); // 如果使用凭证登录，包含用户提交的凭证
             return true; // 如果返回 `false`，则登录会被拒绝
-          }
+          },
+        async jwt({ token, user, account }) {
+            // account 第一次 github 登录时会存在，之后触发这个 jwt 函数，account 会为空
+            if (account) {
+                console.log("account is true")
+                token.accessToken = account.access_token; // 保存 access_token
+            }
+            console.log("JWT Callback:", { token, user, account });
+            return token;
+        },
+        async session({ session, token }) {
+            console.log("Session Callback before:", { session });
+            session.accessToken = token.accessToken; // 将 access_token 添加到 session
+            console.log("Session Callback added accessToken:", { session, token });
+            return session;
+          },
           
     },
     secret: process.env.NEXTAUTH_SECRET, //  <---  这里添加 secret
