a71c12f6e3236ad185b74637bf80e8659a6716e1
qyzhizi <2830942138@qq.com>
2025-01-14 15:08:53 +0800

    update

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -13,6 +13,17 @@ const handler = NextAuth({
             clientSecret: process.env.GOOGLE_SECRET || '',
         }),
     ],
+    callbacks: {
+        async signIn({ user, account, profile, email, credentials }) {
+            console.log(user); // 包含用户基本信息
+            console.log(account); // 包含 OAuth 提供者的认证信息
+            console.log(profile); // OAuth 提供者返回的用户资料
+            console.log(email); // 如果使用邮件登录，包含邮箱信息
+            console.log(credentials); // 如果使用凭证登录，包含用户提交的凭证
+            return true; // 如果返回 `false`，则登录会被拒绝
+          }
+          
+    },
     secret: process.env.NEXTAUTH_SECRET, //  <---  这里添加 secret
 })
 
